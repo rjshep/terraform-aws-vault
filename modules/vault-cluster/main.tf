fe283cc936299c0bdab37fd41ccaa38e9ecea0d4
Richard Shepherd <richard@fibratusltd.co.uk>
2019-09-02 12:40:41 +0100

    Remove all outbound access sg rule

--- modules/vault-cluster/main.tf
+++ modules/vault-cluster/main.tf
@@ -170,16 +170,6 @@ resource "aws_security_group_rule" "allow_ssh_inbound_from_security_group_ids" {
   security_group_id = aws_security_group.lc_security_group.id
 }
 
-resource "aws_security_group_rule" "allow_all_outbound" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.lc_security_group.id
-}
-
 # ---------------------------------------------------------------------------------------------------------------------
 # THE INBOUND/OUTBOUND RULES FOR THE SECURITY GROUP COME FROM THE VAULT-SECURITY-GROUP-RULES MODULE
 # ---------------------------------------------------------------------------------------------------------------------
